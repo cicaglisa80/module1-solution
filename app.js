47bb487383c8d2416501bad5c729d78830261b0e
Milos Kirjakovic <29dec1980@gmail.com>
2017-01-20 16:00:29 +0000

    - Added coloring of labels when message is shown

--- app.js
+++ app.js
@@ -6,17 +6,21 @@
   LunchCheckController.$inject = ['$scope'];
 
   function LunchCheckController($scope) {
+
+    $scope.customStyle = {};
     $scope.outputMessage = '';
 
     $scope.checkAmountOfFood = function () {
 
       if (!$scope.food) {
+        $scope.customStyle.colorClass = "red";
         $scope.outputMessage = 'Please enter data first';
         return;
       }
 
       var result = $scope.food.split(',');
 
+      $scope.customStyle.colorClass = "green";
       if (result.length <= 3) {
         $scope.outputMessage = 'Enjoy!';
       } else {
